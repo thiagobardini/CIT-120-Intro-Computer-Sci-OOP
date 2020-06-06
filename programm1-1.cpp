2804d8c79a5bcb6131dfe569e8f43da67f469dd6
Thiago Bardini <thiagobardini@hotmail.com>
2020-06-06 13:14:50 -0400

    test

---
+++ programm1-1.cpp
@@ -0,0 +1,22 @@
+// This programm calculates the user's pay.
+#include <iostream>
+
+int main()
+{
+    double hours, rate, pay;
+
+    // Get the number hours worked.
+    count << "How many hours did you work?"
+    cin >> hours;
+
+    // Get the hourly pay rate.
+    cout << "How much do you get paid per hour? ";
+    cin >> rate;
+
+    // Calculate the pay
+    pay = hours * rate;
+
+    // Display the pay.
+    cout << "You have earned $"<< pauy << endl;
+    return 0;
+}
