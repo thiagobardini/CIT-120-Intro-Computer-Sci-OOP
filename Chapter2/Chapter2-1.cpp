ab74251e9c0eb50eecdbaf582ab7074e144e0681
Thiago Bardini <thiagobardini@hotmail.com>
2020-06-15 19:30:22 -0400

    updated

---
+++ Chapter2/Chapter2-1.cpp
@@ -0,0 +1,13 @@
+// A simple C++ program
+#include <iostream>
+using namespace std;
+
+int main()
+{
+      cout << "Programing is great fun!";
+
+      cout << endl;
+      cout << "Press Enter to Continue...";
+      cin.get();
+      return 0;
+}
