f666828116ab4aa3c1b7c839fd4a3a1ebd49d034
Thiago Bardini <thiagobardini@hotmail.com>
2020-07-05 13:31:38 -0400

    updated

---
+++ Chapter5/program5-5.cpp
@@ -0,0 +1,69 @@
+//
+//  main.cpp
+//  program5-5
+//
+//  Created by Thiago Bardini on 7/5/20.
+//
+// This program calculates the number of soccer team
+// that a youth league may create from the number of
+// available players. Input validation is demostrated
+// with while loops.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Constants for minimum and maximum players
+    const int MIN_PLAYERS = 9,
+              MAX_PLAYERS = 15;
+    
+    // Variables
+    int players,        // Number of available players
+        teamPlayers,    // Number od desired players per team
+        numTeams,       // Number of teams
+        leftOver;       //Number of players left over
+    
+    // Get the number of players per team.
+    cout << "How many players do you wish per team? ";
+    cin >> teamPlayers;
+    
+    // Validate the input
+    while (teamPlayers < MIN_PLAYERS || teamPlayers > MAX_PLAYERS)
+    {
+        // Explain the error.
+        cout << "You should have at least "<< MIN_PLAYERS <<" but no more than "
+             << MAX_PLAYERS <<" per team.\n";
+    
+        // Get the input again
+        cout << "How many players do you wish per team? ";
+        cin >> teamPlayers;
+    }
+    
+    // Get the number of players available.
+    cout << "How many players are available? ";
+    cin >> players;
+    
+    // Validate the input
+    while (players <= 0)
+    {
+        // Get the input again.
+        cout << "Please enter 0 or greater: ";
+        cin >> players;
+    }
+    
+    // Calculate the number of teams.
+    numTeams = players / teamPlayers;
+    
+    // Calculate the number of leftover players.
+    leftOver = players % teamPlayers;
+    
+    // Display the results
+    cout << "There will be " << numTeams << " teams with "
+         << leftOver << " players left over.\n";
+    
+    cout << endl;
+    cout << "Press Enter to Continue...";
+    cin.get();
+    return 0;
+}
